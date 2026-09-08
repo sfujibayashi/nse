9d5260e4b93b645fa7e38315d94c20c18bf07968
Sho Fujibayashi <sho.fujibayashi@gmail.com>
2026-09-08 13:10:47 +0900

    NSE calculator at a single point (rho,T,Ye)

---
+++ src/nse_single.f90
@@ -0,0 +1,60 @@
+program nse_single
+  use module_nse
+  use module_ptf_reaclib
+  use module_ptf_rauscher
+  implicit none
+  
+  real(8) :: rho, temp, ye
+  integer,parameter :: itrlim = 300
+  real(8),parameter :: tol = 1d-10
+
+  integer :: n_spec
+  real(8),allocatable :: xnse(:)
+
+  logical :: nsefail, use_TNAguess
+  
+  real(8) :: mexc_ave, z_heavy, a_heavy, y_heavy, ytot, xsum, yesum, x_heavy
+
+  integer,parameter :: n_rank = 10
+  integer :: index_r(n_rank)
+  integer :: i
+  
+  character(256) :: fn_out
+
+  use_TNAguess = .false.
+
+  block
+    character(256) :: fn_para
+    character(256) :: fn_winv, fn_raucher
+    logical :: use_rauscher_ptf
+    
+    call getarg(1, fn_para)
+  
+    open(10,file=fn_para,status="old",action="read")
+    read(10,*);read(10,'(a)') fn_winv
+    read(10,*);read(10,'(L)') use_rauscher_ptf
+    read(10,*);read(10,'(a)') fn_raucher
+    read(10,*);read(10,'(a)') fn_out
+    read(10,*);read(10,*) rho, temp, ye
+    close(10)
+    
+    call init_ptf_reaclib(fn_winv)
+    call init_ptf_rauscher(fn_raucher)
+    call nse_init_reaclib(n_spec, use_rauscher_ptf)
+
+  end block
+
+  allocate(xnse(n_spec))
+
+  call calc_nse(rho,temp,ye,itrlim,tol,xnse,nsefail,use_TNAguess)
+  
+  call statistic(xnse, mexc_ave, z_heavy, a_heavy, y_heavy, ytot, xsum, yesum)
+  x_heavy = a_heavy*y_heavy
+  call index_rank(n_rank, n_spec, xnse, index_r, 1d0)
+  
+  write(6,'(a, 11es16.7e3)') "rho, T, Ye = ", rho,temp,ye
+  write(6,'(10a16,10es16.7e3)') (name_reaclib(ireaclib(index_r(i))),i=1,n_rank), (xnse(index_r(i)),i=1,n_rank)
+
+  call output_nse_full(rho,temp,ye,xnse, fn_out)
+  
+end program nse_single
